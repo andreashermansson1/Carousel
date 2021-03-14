5e197e333ea49532e527cd045ba5c157982a81e5
Andreas <andreas.hermansson2@gmail.com>
2021-03-14 18:39:11 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home'
+import About from './About'
+import { Route, Switch, Link, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
+
     <div className="App">
-      <p>Hej!</p>
+          <BrowserRouter>
+    <div>
+      <nav>
+        <ul id="navigation">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+          <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+      <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+    </Switch>
+    </BrowserRouter>
     </div>
   );
 }
